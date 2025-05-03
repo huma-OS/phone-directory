6b0a8748d1fea9c58ceb8748b5d5dd5da4fbe708
huma-OS <cheifomera@yahoo.com>
2025-05-03 16:02:24 +0100

    0002-funtionality only

--- scripts.js
+++ scripts.js
@@ -1,5 +1,67 @@
-function sum(a, b) {
-  return a + b;
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// module.exports = sum;
+
+
+const nameInput = document.getElementById('name');
+const mobileInput = document.getElementById('mobile');
+const emailInput = document.getElementById('email');
+const errDiv = document.getElementById('error');
+const submitBtn = document.getElementById('submit');
+
+let valid = true;
+
+const nameRegex = /^[A-Za-z\s]{1,20}$/;
+const mobileRegex = /^[0-9]{10,11}$/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+function validateField(input, regex) {
+  if (!regex.test(input.value.trim())) {
+    input.classList.add('input-error');
+  } else {
+    input.classList.remove('input-error');
+  }
 }
 
-module.exports = sum;
+nameInput.addEventListener('input', () => validateField(nameInput, nameRegex));
+mobileInput.addEventListener('input', () => validateField(mobileInput, mobileRegex));
+emailInput.addEventListener('input', () => validateField(emailInput, emailRegex));
+
+
+submitBtn.addEventListener('click', function () {
+  const name = nameInput.value.trim();
+  const mobile = mobileInput.value.trim();
+  const email = emailInput.value.trim();
+
+  const validName = nameRegex.test(name);
+  const validMobile = mobileRegex.test(mobile);
+  const validEmail = emailRegex.test(email);
+
+  if (!validName || !validMobile || !validEmail) {
+    errDiv.style.display = 'block';
+    return;
+  }
+
+  errDiv.style.display = 'none';
+
+  const defaultRow = document.getElementById('defaultRow');
+  if (defaultRow) {
+    defaultRow.remove();
+  }
+
+  nameInput.value = '';
+  mobileInput.value = '';
+  emailInput.value = '';
+  nameInput.classList.remove('input-error');
+  mobileInput.classList.remove('input-error');
+  emailInput.classList.remove('input-error');
+
+  const tableBody = document.querySelector('#summaryTable tbody');
+  const newRow = tableBody.insertRow();
+  newRow.insertCell(0).innerHTML = `<img src="./img/user.png" alt="Name Icon" style="width:30px; vertical-align:middle; margin-right:5px;"> ${name}`;
+  newRow.insertCell(1).innerHTML = `<img src="./img/call.png" alt="Name Icon" style="width:30px; vertical-align:middle; margin-right:5px;"> ${mobile}`;
+  newRow.insertCell(2).innerHTML = `<img src="./img/email.png" alt="Name Icon" style="width:30px; vertical-align:middle; margin-right:5px;"> ${email}`;
+});
